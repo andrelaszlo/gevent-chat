2868e244d6c12a44b059a60c65fcebfe4fd0b39f
André Laszlo <andre@laszlo.nu>
2015-02-11 22:26:38 +0100

    Adding messages from input to output

--- client.py
+++ client.py
@@ -11,10 +11,24 @@ class ChatMessages(ListBox):
         self.walker.append(Text(message))
         self.set_focus(len(self.walker)-1)
 
+class ChatInput(Edit):
+
+    def setup(self, messages):
+        self.messages = messages
+        return self
+
+    def keypress(self, size, key):
+        if key == 'enter':
+            self.messages.add(self.get_edit_text())
+            self.set_edit_text('')
+        super(ChatInput, self).keypress(size, key)
+
+output = ChatMessages()
 for i in range(100):
     output.add("Test message %d" % i)
 
-message = Edit(caption='> ', edit_text="")
+message = ChatInput(caption='> ').setup(output)
+
 window = Frame(body=output, footer=message, focus_part='footer')
 main_loop = MainLoop(window, event_loop=GeventLoop())
 #gevent.spawn(background_loop)
